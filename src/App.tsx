55f3e7f0eb795c727d0603e5e69c63a56ee3c868
Lautaro Soffici <lautarosoffici@gmail.com>
2021-01-30 15:47:10 -0300

    delete header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Header from './components/Header/Header';
 import Location from './components/Location/Location';
 
 const App: React.FC = () => (
-  <div>
-    <Header />
-    <Location />
-  </div>
+  <Location />
 );
 
 export default App;
